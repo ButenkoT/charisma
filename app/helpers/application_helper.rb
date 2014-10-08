39f57debe028056ae42f0b0bbd10965b6b3d324a
ButenkoT <tanya@butenko.me>
2014-10-08 17:30:03 +1100

    main page styling css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,10 @@ module ApplicationHelper
     nav = ''
     if @current_user.present?
       nav += "<li>Hello #{ @current_user.name }</li> "
-      nav += "<li>" + link_to('All users', users_path) + "</li> "
-      nav += "<li>" + link_to('Your profile', user_path(@current_user)) + "</li> "
+      nav += "<li>" + link_to('Users', users_path) + "</li> "
+      nav += "<li>" + link_to('Profile', user_path(@current_user)) + "</li> "
       nav += "<li>#{ link_to('Sign out', login_path, :method => :delete, :data => {:confirm => 'Are you sure?'}) }</li> "
-      nav += "<li>" + link_to('Add product', new_product_path) + "</li> "
+      nav += "<li>" + link_to('Add Item', new_product_path) + "</li> "
     else
       nav += "<li>#{ link_to('Sign up', new_user_path) }</li> "
       nav += "<li>#{ link_to('Sign in', login_path) }</li>"
